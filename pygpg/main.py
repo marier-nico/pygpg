fec26c57bfa775e863c557e4f1a70a1ba10b8c98
Nicolas Marier <software@nmarier.com>
2020-11-27 21:25:33 -0500

    Add flake8 and pylint

--- pygpg/main.py
+++ pygpg/main.py
@@ -1,8 +1,10 @@
+"""Main entrypoint for the CLI."""
 import click
 
 
 @click.command()
 def main():
+    """Main command to invoke with the CLI."""
     click.secho("Hello, World!", fg="green")
 
 
